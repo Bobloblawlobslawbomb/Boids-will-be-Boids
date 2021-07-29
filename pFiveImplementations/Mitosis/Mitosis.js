aafcc67d2dc8661ea2d1bdf782a81a76bf7126dd
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-07-28 22:24:53 -0700

    small styling tweaks

--- pFiveImplementations/Mitosis/Mitosis.js
+++ pFiveImplementations/Mitosis/Mitosis.js
@@ -4,6 +4,10 @@ function setup() {
   createCanvas(1000,1000)
   cells.push(new Cell())
   cells.push(new Cell())
+  cells.push(new Cell())
+  cells.push(new Cell())
+  cells.push(new Cell())
+  cells.push(new Cell())
 
 }
 
